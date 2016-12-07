0d7c364cb3fe7d99e14dfd7828783a495fe62cef
takao saori <takao-saori@av.avex.co.jp>
2016-12-07 17:43:13 +0000

    実習01-14

---
+++ app/controllers/timelines_controller.rb
@@ -0,0 +1,66 @@
+class TimelinesController < ApplicationController
+  
+  def index
+     # メッセージ入力
+    @input_message = params[:id] ? Timeline.find(params[:id]) : Timeline.new
+    @timeline = Timeline.includes(:user).not_reply.user_filter(params[:filter_user_id]).order('updated_at DESC')
+    @users = User.all
+    
+    if params[:reply_id]
+      # 返信時は返信のタイムライン情報を取得
+      @reply_timeline = Timeline.find(params[:reply_id])
+    end
+    
+  end
+  
+  def create
+    timeline = Timeline.new
+    timeline.attributes = input_message_param
+    timeline.user_id = current_user.id
+    if timeline.valid? # バリデーションチェック
+      timeline.save!
+    else
+      flash[:alert] = timeline.errors.full_messages
+    end
+    unless request.format.json?
+      redirect_to action: :index
+    else
+      # ajaxの場合のレスポンス
+      html = render_to_string partial: 'timelines/timeline', layout: false, formats: :html, locals: { t: timeline }
+      render json: {timeline: html}
+    end
+  end
+  
+  def update
+    timeline = Timeline.find(params[:id])
+    timeline.attributes = input_message_param
+    if timeline.valid? # バリデーションチェック
+      timeline.save!
+    else
+      flash[:alert] = timeline.errors.full_messages
+    end
+    redirect_to action: :index
+  end
+  
+  def destroy
+    timeline = Timeline.find(params[:id])
+    timeline.destroy
+    redirect_to action: :index
+  end
+  
+  def filter_by_user
+    if params[:filter_user_id].present?
+      redirect_to action: :index, filter_user_id: params[:filter_user_id]
+    else
+      # フィルターなし
+      redirect_to action: :index
+    end
+  end
+  
+
+  private
+  def input_message_param
+    params.require(:timeline).permit(:message, :reply_id)
+  end
+  
+end
---
+++ app/models/timeline.rb
@@ -0,0 +1,20 @@
+class Timeline < ActiveRecord::Base
+    belongs_to :user
+    
+    belongs_to :parent,  class_name: 'Timeline', foreign_key: 'reply_id'
+    
+    has_many   :replies, class_name: 'Timeline', foreign_key: 'reply_id', dependent: :destroy
+    
+    validates :message, presence: true, allow_blank: false
+    
+    scope :user_filter, -> user_id do
+     # user_idパラメータがあればフィルターをかける
+     where(user_id: user_id) if user_id.present?
+    end
+  
+    scope :not_reply, ->  do
+     # 返信でないデータを取得
+     where(reply_id: nil)
+    end
+    
+end
